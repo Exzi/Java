b4d04078dc813fe02a3433c82843fdabba31238f
Ilya <iliya176@yandex.ru>
2024-10-14 17:38:01 +0300

    Добавлен метод removeNegativeNumbers для удаления отрицательных чисел

--- L3_1.java
+++ L3_1.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class L3_1 {
     public static void main(String[] args) {
+        List<Integer> numbers = List.of(-1,2,-3,4,-5,6)
+        List<Integer> result = removeNegativeNumbers(numbers);
+        System.out.println("Результат: "+ result);
+    }
 
+    public static List<Integer> removeNegativeNumbers(List<Intereg> list) {
+        List<Intereg> positiveNumbers = new ArrayList<>();
+        for (int num : list) {
+            if (num >=0){
+                positiveNumbers.add(num);
+            }
+        }
+        return positiveNumbers;
     }
 }
