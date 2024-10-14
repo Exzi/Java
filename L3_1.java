fb5f35eac82fb8adf1f23bcfef8028b796933ec3
Ilya <iliya176@yandex.ru>
2024-10-14 17:44:44 +0300

    Исправлены ошибки в коде

--- L3_1.java
+++ L3_1.java
@@ -3,15 +3,16 @@ import java.util.List;
 
 public class L3_1 {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(-1,2,-3,4,-5,6)
+        List<Integer> numbers = List.of(-1, 2, -3, 4, -5, 6);
         List<Integer> result = removeNegativeNumbers(numbers);
-        System.out.println("Результат: "+ result);
+        System.out.println("Результат: " + result);
     }
 
-    public static List<Integer> removeNegativeNumbers(List<Intereg> list) {
-        List<Intereg> positiveNumbers = new ArrayList<>();
+    
+    public static List<Integer> removeNegativeNumbers(List<Integer> list) {
+        List<Integer> positiveNumbers = new ArrayList<>();
         for (int num : list) {
-            if (num >=0){
+            if (num >= 0) {
                 positiveNumbers.add(num);
             }
         }
